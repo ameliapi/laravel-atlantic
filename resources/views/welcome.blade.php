7c393e9923780cab9f4e5756337bd83b2c897e73
ameliapi <ameliapi403@gmail.com>
2018-03-06 21:15:31 -0700

    Uploaded the Atlantic Project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,74 @@
+@extends('layout')
+
+@section('content')
+
+	<header class="">
+
+		<div class = "logoContainer">
+			<img class="logo" src="img/logo.png" alt="The Atlantic Magazine Logo"/>
+		</div>
+
+		<div class = "menuContainer">
+			<li class="menu">Popular</li>
+			<li class="menu">Latest</li>
+			<li class="menu">Sections</li>
+			<li class="menu">Magazine</li>
+			<li class="menu">More</li>
+			<li class="menu">Subscribe</li>
+			<i class="fa fa-search" aria-hidden="true" style="color:white;"></i>
+		</div>
+
+	</header>
+
+
+	<section class="heroContainer">
+		<div class="heroLeft">
+			<ul>
+		    <?php foreach ($artLeft as $article): ?>
+		        <li class="artL">
+		        	<img class="avatarLeft" src="<?php echo $article->header_img; ?>" alt=""/><br/>
+		            <h4><?php echo $article->title; ?></h4><br/>
+		            <h6><?php echo $article->abstract; ?></h6>
+	      			<h6><?php echo $article->user->name; ?></h6>
+
+		        </li>
+		    <?php endforeach; ?>
+	    	</ul>
+		</div>
+
+		<div class="heroCenter">
+			<ul>
+		    <?php foreach ($artCenter as $article): ?>
+		        <li class="artC">
+		        	<img class="avatarCenter" src="<?php echo $article->header_img; ?>" alt=""/><br/>
+		            <h2><?php echo $article->title; ?></h2><br/>
+		            <h6><?php echo $article->abstract; ?></h6>
+	      			<h6><?php echo $article->user->name; ?></h6>
+		        </li>
+		    <?php endforeach; ?>
+	    	</ul>
+		</div>
+
+		<div class="heroRight">
+			<ul>
+		    <?php foreach ($artRight as $article): ?>
+		        <li class="artR">
+		        	<div class="artInfo-s">
+		        		
+			            <h6><?php echo $article->title; ?></h6><br/>
+			            <?php echo $article->abstract; ?>
+		      			<?php echo $article->user->name; ?>
+		      			
+	      			</div>
+
+	      			<div class="avatarRight">
+		        		<img id="icon-s" src="<?php echo $article->header_img; ?>" alt=""/><br/>
+		        	</div>		        	
+		        </li>
+		    <?php endforeach; ?>
+	    	</ul>
+		</div>
+
+	</section>
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Faker\Factory;
+
+class User{
+
+	public $name;
+	public $image;
+	public $bio;
+	public $date_joined;
+
+	public function __construct($name, $image){
+		$this->name = $name;
+		$this->image = $image;
+	}
+
+ }
+
+class Article{
+
+	public $user;
+	public $time;
+	public $title;
+	public $header_img;
+	public $abstract;
+	public $content;
+
+	public function __construct($user, $title, $header_img){
+		$this->user = $user;
+		$this->title = $title;
+		$this->header_img = $header_img;
+	}
+
+}
+
+
+
+Route::get('/about', function(){
+	return view('about');
+});
+
+Route::get('/', function () {
+
+	$faker = Factory::create();
+
+	$user1 = new User($faker->name, $faker->imageUrl());
+	$user2 = new User($faker->name, $faker->imageUrl());
+	$user3 = new User($faker->name, $faker->imageUrl());
+	$user4 = new User($faker->name, $faker->imageUrl());
+	$user5 = new User($faker->name, $faker->imageUrl());
+	$user6 = new User($faker->name, $faker->imageUrl());
+	$user7 = new User($faker->name, $faker->imageUrl());
+
+	$article1 = new Article($user1, $faker->sentence, $faker->imageUrl());
+	$article2 = new Article($user2, $faker->sentence, $faker->imageUrl());
+	$article3 = new Article($user3, $faker->sentence, $faker->imageUrl());
+	$article4 = new Article($user4, $faker->sentence, $faker->imageUrl());
+	$article5 = new Article($user5, $faker->sentence, $faker->imageUrl());
+	$article6 = new Article($user6, $faker->sentence, $faker->imageUrl());
+	$article7 = new Article($user7, $faker->sentence, $faker->imageUrl());
+
+	$data = [
+		'users' => [$user1, $user2, $user3, $user4, $user5, $user6, $user7],
+		'artLeft' => [$article1, $article2],
+		'artCenter' => [$article3],
+		'artRight' => [$article4, $article5, $article6, $article7],
+	];
+
+    return view('welcome', $data);
+
+});
