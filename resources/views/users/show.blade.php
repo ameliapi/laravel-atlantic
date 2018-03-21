1360ceed0f0ff36e55b29e688ce66e2e61c4ea46
ameliapi <ameliapi403@gmail.com>
2018-03-21 00:51:19 -0600

    updated user page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,34 +6,39 @@
 
     <div class="col-sm-8">
         <ul class="blog">
-        @foreach ($user->articles as $article)
-            <article>
-                <li class="card">
-                    <img class="avatarLeft" src="{{ $article->image }}" alt=""/><br/><br/>
-                    <h4>{{ $article->title }}</h4><br/>
-                    {{ $article->updated_at->toFormattedDateString() }}<br/>
-                    <strong>Abstract: </strong>
-                    {{ $article->abstract }}<br/><br/>
-                    {{ $article->content }}<br/><br/>
-                </li>
-            </article>
-        @endforeach
+            @if ($user->articles->count() > 0)
+                <h5>@ {{ $user->name }} published: </h5>
+                @foreach ($user->articles as $article)
+                    <article>
+                        <li class="card">
+                            <img class="avatarLeft" src="{{ $article->image }}" alt=""/><br/><br/>
+                            <h4>{{ $article->title }}</h4><br/>
+                            {{ $article->updated_at->toFormattedDateString() }}<br/>
+                            <strong>Abstract: </strong>
+                            {{ $article->abstract }}<br/><br/>
+                            {{ $article->content }}<br/><br/>
+                        </li>
+                    </article>
+                @endforeach
+            @endif
         </ul>
     </div>
 
     <br/>
-                
-    <h5>@ {{ $user->name }} commented: </h5>
-    <div class="comments">
-        <ul class="list-group">
-        @foreach ($user->comments as $comment)    
-            <li class="list-group-item">
-                  {{ $comment->content }}<br/>
-                  <strong>{{ $comment->created_at->diffForHumans() }} </strong>
-            </li>
-        @endforeach
-        </ul>
-    </div>
+    
+    @if ($user->comments->count() > 0)           
+        <h5>@ {{ $user->name }} commented: </h5>
+        <div class="comments">
+            <ul class="list-group">
+            @foreach ($user->comments as $comment)    
+                <li class="list-group-item">
+                      {{ $comment->content }}<br/>
+                      <strong>{{ $comment->created_at->diffForHumans() }} </strong>
+                </li>
+            @endforeach
+            </ul>
+        </div>
+    @endif
 
     <br/>
 
